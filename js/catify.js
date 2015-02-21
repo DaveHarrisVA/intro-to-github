3b0b1734f8948ea811503ccb76bb9a184ffc804e
Dave Harris <dave@jupiterduo.com>
2015-02-21 14:48:44 -0500

    Update and rename js to js/catify.js
    Javascript library to replace all images in any given webpages with those of cats

---
+++ js/catify.js
@@ -0,0 +1,13 @@
+//Catify.js
+//Javascript library to replace all images in any given webpages with those of cats
+//Wouldn't that be cute?  Would be a good public wi-fi prank.
+
+
+//Sources for cat images to replace with go here.  Images randomly selected from this list.
+Array Images
+{
+  image1.png,
+  image2.png,
+  image3.png
+
+}
